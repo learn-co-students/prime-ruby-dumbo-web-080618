90721bd854ad12209485bd15d71b80a0af552283
Otabek Kamalov <otakam86@gmail.com>
2018-07-21 14:44:08 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,23 @@
-# Add  code here!
+def prime? (number)
+  
+  if number <= 1 
+    
+    result = false 
+  elsif number == 2 || number == 3 || number == 5 || number== 7
+    result = true
+  else  
+     i = 2
+     while i < number 
+      
+       if number % i == 0 
+          result = false
+          break
+		   else
+		  	  result = true
+       end 
+        	i += 1 
+
+     end 
+  end
+ return result
+end
