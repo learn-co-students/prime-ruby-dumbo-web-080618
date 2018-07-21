9978a66f0c77bf7e5e42a387da9b7b72d7cf4e7f
Brennan Basinger <bdbasinger777@gmail.com>
2018-07-21 15:50:12 -0500

    Finished

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(num)
+  if num == 2
+    return true
+  end
+  if num < 2
+    return false
+  end
+  arr = (2..(num - 1)).to_a
+  arr.each do |x|
+    puts "#{num} % #{x} == #{num % x}"
+    if num % x == 0
+      return false
+    end
+  end
+  return true
+end
+
