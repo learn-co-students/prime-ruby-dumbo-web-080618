e385f099d5d787d2e09446acae723f3962bcf24c
joyhuangg <joyhuangg@gmail.com>
2018-07-28 01:02:38 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1
+    return false
+  end
+  divisor = 2
+  while divisor <= Math.sqrt(num)
+    if num%divisor == 0
+      return false
+    end
+    divisor += 1
+  end
+  return true
+end
