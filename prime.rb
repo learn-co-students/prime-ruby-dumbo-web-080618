4c68f2fa97d988ded1d3c76c9cae8150ad38e772
shemarjoseph <shemarjoseph@yahoo.com>
2018-07-28 12:42:56 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
 # Add  code here!
+def prime?(n)
+    i = 2
+
+    if n <= 1
+        return false
+    end
+
+    if n == 2
+        return true
+    end
+
+    sqrt_n = Math.sqrt(n)
+    while i <= sqrt_n
+        if n % i == 0 
+        return false
+        end
+        i += 1
+    end
+
+    return true
+end
